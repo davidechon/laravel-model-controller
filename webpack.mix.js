c3d9845cf397f7730d0d626c4e4ae6ca2a12de4f
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-06-13 15:10:39 +0200

    scaffolding

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').options({
+        processCssUrls: false}).copyDirectory('.node_modules/@fortawesome/forntawesome-free/webfonts/*', 'public/fonts/-font-awesome');
